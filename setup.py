fc46d3b2bf4c4e7d3c8f2f06c4c108690da053b9
Yara Fekry <36644314+YaraFekri@users.noreply.github.com>
2021-01-24 16:24:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from distutils.core import setup
 setup(
-  name = 'testt',         # How you named your package folder (MyLib)
-  packages = ['testt'],   # Chose the same as "name"
+  name = 'Neural_Networks',         # How you named your package folder (MyLib)
+  packages = ['Neural_Networks'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'Yara',                   # Type in your name
-  author_email = 'yaraismailf@gmail.com',      # Type in your E-Mail
+  author = 'YOUR NAME',                   # Type in your name
+  author_email = 'yarafekrii@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/nadayehia20/Neural_Networks',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['random', 'choices', 'magic'],   # Keywords that define your package best
-  install_requires=[      'numpy', 'pandas', 'pickle','matplotlib'      # I get to this in a second
+  keywords = ['Neural', 'Networks'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'pandas','matplotlib','pickle'
+          
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
@@ -22,5 +25,6 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    
   ],
 )
