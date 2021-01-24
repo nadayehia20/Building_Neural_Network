60023cb172fa7d7ea99b8c2459227e18ced2d699
Nadien-Rashad <42560282+Nadien-Rashad@users.noreply.github.com>
2021-01-24 19:33:45 +0200

    Create Add Layer.py

---
+++ Seperate modules/Add Layer.py
@@ -0,0 +1,20 @@
+def add_layer( units, activation='tanh', initializer='random',layer_units[],layer_activations[],layer_initializers[]):
+
+        '''
+        module that builds a layer knowing number of neurons in the layer,
+        the activation function that will be used to it and the initializer type for weights/bias 
+        :param units: number of neurons in this layer
+        :param activation: Activation function for the neurons in this layer. 
+                           Current options are: tanh, sigmoid, softmax, reLU, leaky reLU
+           
+        :param initializer: Algorithm to initialize weights/biases of the NN.
+                            Current options are:
+                           'zeros' : Sets all weights/biases to zero
+                           'random' : Sets weights/biases to random numbers between 0 and 1, from a uniform distribution
+                           
+        :param layer_units: array
+        '''
+
+        layer_units.append(units)
+        layer_activations.append(activation)
+        layer_initializers.append(initializer)
