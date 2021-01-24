c4804f6058aac93cd6da54fe8bad1c42d0eaa260
Nadien-Rashad <42560282+Nadien-Rashad@users.noreply.github.com>
2021-01-24 19:52:08 +0200

    Update Add Layer.py

--- Seperate modules/Add Layer.py
+++ Seperate modules/Add Layer.py
@@ -1,4 +1,4 @@
-def add_layer( units, activation='tanh', initializer='random',layer_units[],layer_activations[],layer_initializers[]):
+def add_layer( units, activation='tanh', initializer='xavier',layer_units[],layer_activations[],layer_initializers[]):
 
         '''
         module that builds a layer knowing number of neurons in the layer,
@@ -9,10 +9,14 @@ def add_layer( units, activation='tanh', initializer='random',layer_units[],laye
            
         :param initializer: Algorithm to initialize weights/biases of the NN.
                             Current options are:
+                           'xavier' : Sets weights/biases to numbers sampled from a normal distribution, with variance = 1/n_{l-1}
                            'zeros' : Sets all weights/biases to zero
                            'random' : Sets weights/biases to random numbers between 0 and 1, from a uniform distribution
                            
-        :param layer_units: array
+                           
+        :param layer_activation: array has the activation function name that will be used for the layer
+               layer_initializers: array has the type of weights initiaizations of each layer
+               layer_units: array has the number of neuron units in each layer
         '''
 
         layer_units.append(units)
