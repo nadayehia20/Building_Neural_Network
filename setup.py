b2f7dc5350be63c447829c9df9a4d8de8bb7d513
Yara Fekry <36644314+YaraFekri@users.noreply.github.com>
2021-01-24 16:18:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   url = 'https://github.com/nadayehia20/Neural_Networks',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['random', 'choices', 'magic'],   # Keywords that define your package best
-  install_requires=[      'numpy, 'pandas', 'pickle'      # I get to this in a second
+  install_requires=[      'numpy', 'pandas', 'pickle'      # I get to this in a second
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
