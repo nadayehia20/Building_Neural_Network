e4ea2afce3e5bf37c321c15b54e29c0673f1fd63
Yara Fekry <36644314+YaraFekri@users.noreply.github.com>
2021-01-24 17:03:08 +0200

    Delete Building_Neural_Network directory

--- Building_Neural_Network/utils.py
+++
@@ -1,9 +0,0 @@
-class utils():
-  def load_model(self,filename):
-    import pickle 
-    loaded_model = pickle.load(open(filename, 'rb'))
-    return loaded_model
-
-  def save_model(self,nn,filename):
-    import pickle 
-    pickle.dump(nn, open(filename, 'wb'))
