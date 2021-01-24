84b66f0ff4dc3c55d0140a58fdeab44b54d33e71
Nadien-Rashad <42560282+Nadien-Rashad@users.noreply.github.com>
2021-01-24 18:58:58 +0200

    Update initialize parameters.py

--- Seperate modules/initialize parameters.py
+++ Seperate modules/initialize parameters.py
@@ -1,34 +1,43 @@
 
-    def initialize_params(self, X_input):
+    def initialize_params( X_input,layer_units[],layer_initializers[],W,b,Z,A):
 
        '''
         Initializes weight and bias matrices for training, using the desired initialization algorithm(s) 
         that will be chosen by the user whether to choose  to initialize weights by zeros or random intialization.
 
         :param X_input: Training set
-        '''
-        import numpy as np
+               W:  weights of the layers
+               b:  bias of the layers 
+               Z:
+               A:
+               layer_initializers: array has the type of weights initiaizations of each layer
+               layer_units: array has the number of neuron units in each layer
 
-        self.layer_units[0] = X_input.shape[0]
+        
+         
+        '''
+        
+
+        layer_units[0] = X_input.shape[0]
         m = X_input.shape[-1]   
 
         num_layers = len(self.layer_units) - 1
         for l in range(1, num_layers+1):
-            n_l = self.layer_units[l]
-            n_prev = self.layer_units[l-1]
+            n_l = layer_units[l]
+            n_prev = layer_units[l-1]
 
-            if self.layer_initializers[l] == 'zeros':
-                self.W.append(np.zeros(shape=(n_l, n_prev), dtype=np.float32))
-                self.b.append(np.zeros(shape=(n_l, 1), dtype=np.float32))
+            if layer_initializers[l] == 'zeros':
+                W.append(np.zeros(shape=(n_l, n_prev), dtype=np.float32))
+                b.append(np.zeros(shape=(n_l, 1), dtype=np.float32))
 
             
 
-            elif self.layer_initializers[l] == 'random':
-                self.W.append(np.random.rand(n_l, n_prev).astype(np.float32))
-                self.b.append(np.random.rand(n_l, 1).astype(np.float32))
+            elif layer_initializers[l] == 'random':
+                W.append(np.random.rand(n_l, n_prev).astype(np.float32))
+                b.append(np.random.rand(n_l, 1).astype(np.float32))
 
            
             else:
                 return False
-            self.Z.append(np.zeros(shape=(n_l, m), dtype=np.float32)) 
-            self.A.append(np.zeros(shape=(n_l, m), dtype=np.float32))
+            Z.append(np.zeros(shape=(n_l, m), dtype=np.float32)) 
+            A.append(np.zeros(shape=(n_l, m), dtype=np.float32))
